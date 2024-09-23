0bc70571fd49b8298484e37537a42e39249f9b65
nguyen manh duc <nguyenmanhduc406@gmail.com>
2024-09-24 05:34:47 +0700

    add log home page

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -2,36 +2,43 @@
 export const getTotalSales = async () => {
     let totalOrders = 0
     let totalRevenue = 0
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/orders`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/orders`, { method: "GET", cache: "no-cache" });
     if (!res.ok) {
         console.error(`Failed to fetch orders: ${res.statusText}`);
         return { totalOrders, totalRevenue };
     }
     const orders = await res.json();
+    console.log("orders", orders)
     totalOrders = orders.length;
+    console.log("totalOrders", totalOrders)
     totalRevenue = orders.reduce((acc: number, order: OrderColumnType) => acc + order.totalAmount, 0)
+    console.log("totalRevenue", totalRevenue)
     return { totalOrders, totalRevenue }
 }
 
 export const getTotalCustomers = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/customer`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/customer`, { method: "GET", cache: "no-cache" });
     if (!res.ok) {
         console.error(`Failed to fetch customers: ${res.statusText}`);
         return 0;
     }
     const customers = await res.json();
+    console.log("customers", customers)
     const totalCustomers = customers.length
+    console.log("totalCustomers", totalCustomers)
     return totalCustomers
 }
 
 export const getSalesPerMonth = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/orders`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/orders`, { method: "GET", cache: "no-cache" });
     if (!res.ok) {
+        console.error(`Failed to fetch orders: ${res.statusText}`);
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const a: any[] = []
         return a
     }
     const orders = await res.json();
+    console.log("orders", orders)
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const salesPerMonth = orders.reduce((acc: any, order: any) => {
         const cleanedDateString = order.createdAt.replace(/(\d+)(st|nd|rd|th)/, '$1');
@@ -44,5 +51,6 @@ export const getSalesPerMonth = async () => {
         const month = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(new Date(0, i))
         return { name: month, sales: salesPerMonth[i] || 0 }
     })
+    console.log("graphData", graphData)
     return graphData
 }

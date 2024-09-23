176b6d1ed37b992c94e9b22f4d357d1fb38bd6e8
nguyen manh duc <nguyenmanhduc406@gmail.com>
2024-09-24 06:56:45 +0700

    replace fetch

--- lib/actions/actions.ts
+++ lib/actions/actions.ts
@@ -1,44 +1,52 @@
+import { GetListOrderDocument, GetListUserDocument } from "@/generated/graphql";
+import client from "../apolloClient";
 
 export const getTotalSales = async () => {
     let totalOrders = 0
     let totalRevenue = 0
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/orders`, { method: "GET", cache: "no-cache" });
-    if (!res.ok) {
-        console.error(`Failed to fetch orders: ${res.statusText}`);
+    const GET_ORDERS_QUERY = GetListOrderDocument;
+    const { data, errors } = await client.query({
+        query: GET_ORDERS_QUERY,
+    });
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/orders`, { method: "GET", cache: "no-cache" });
+    if (!data) {
+        console.error(`Failed to fetch orders: ${errors}`);
         return { totalOrders, totalRevenue };
     }
-    const orders = await res.json();
-    console.log("orders", orders)
+    const orders = data.getListOrder.listOrder;
     totalOrders = orders.length;
-    console.log("totalOrders", totalOrders)
     totalRevenue = orders.reduce((acc: number, order: OrderColumnType) => acc + order.totalAmount, 0)
-    console.log("totalRevenue", totalRevenue)
     return { totalOrders, totalRevenue }
 }
 
 export const getTotalCustomers = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/customer`, { method: "GET", cache: "no-cache" });
-    if (!res.ok) {
-        console.error(`Failed to fetch customers: ${res.statusText}`);
+    const GET_USERS_QUERY = GetListUserDocument;
+    const { data, errors } = await client.query({
+        query: GET_USERS_QUERY,
+    });
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/customer`, { method: "GET", cache: "no-cache" });
+    if (!data) {
+        console.error(`Failed to fetch customers: ${errors}`);
         return 0;
     }
-    const customers = await res.json();
-    console.log("customers", customers)
+    const customers = data.getListUser.listUser
     const totalCustomers = customers.length
-    console.log("totalCustomers", totalCustomers)
     return totalCustomers
 }
 
 export const getSalesPerMonth = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/orders`, { method: "GET", cache: "no-cache" });
-    if (!res.ok) {
-        console.error(`Failed to fetch orders: ${res.statusText}`);
+    const GET_ORDERS_QUERY = GetListOrderDocument;
+    const { data, errors } = await client.query({
+        query: GET_ORDERS_QUERY,
+    });
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/orders`, { method: "GET", cache: "no-cache" });
+    if (!data) {
+        console.error(`Failed to fetch orders: ${errors}`);
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const a: any[] = []
         return a
     }
-    const orders = await res.json();
-    console.log("orders", orders)
+    const orders = data.getListOrder.listOrder
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const salesPerMonth = orders.reduce((acc: any, order: any) => {
         const cleanedDateString = order.createdAt.replace(/(\d+)(st|nd|rd|th)/, '$1');
@@ -51,6 +59,5 @@ export const getSalesPerMonth = async () => {
         const month = new Intl.DateTimeFormat('en-US', { month: 'short' }).format(new Date(0, i))
         return { name: month, sales: salesPerMonth[i] || 0 }
     })
-    console.log("graphData", graphData)
     return graphData
 }
